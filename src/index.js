3aa80cf1d3bfb98259bb844e23d5c7033ea0148e
Akash Kumar <122004642+akash-kumar-cmdk@users.noreply.github.com>
2023-08-23 14:58:53 +0530

    cmdk added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CmdKConfigurer } from '@cmdk/stage-toolkit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CmdKConfigurer token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdJZCI6IjIiLCJ1c2VySWQiOiIyIiwiZG9tYWluIjoiaHR0cHM6Ly9sYW5kaW5nLWFrLm5ldGxpZnkuYXBwLyIsImlhdCI6MTY5MjcwNzY4MCwiZXhwIjoxNjkzMzEyNDgwfQ.ZXrOURq_WZZqULu57jP8pI_vnNkletogrh2ODPJgs4Y'>
+      <App />
+    </CmdKConfigurer>
   </React.StrictMode>
 );
 
